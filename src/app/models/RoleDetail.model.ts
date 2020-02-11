9c27f0366b151c5b4ef9326e3a2b73380cf2a21c
Bhagya Shree <shreebshree551@gmail.com>
2020-02-11 21:37:47 +0530

    first commit

---
+++ src/app/models/RoleDetail.model.ts
@@ -0,0 +1,8 @@
+
+export default class RoleDetail {
+    role_id: string;
+    role_name: string;//engineer, representative, electrician etc.
+    allowed_entity_id_arr: string[];//which entities can this role access
+    image_src: string;
+    
+}
